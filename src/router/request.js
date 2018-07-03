1ebfbeca48ea1dc20c6e9ca2adcdab00c0206be4
SML <dltkdals2202@gmail.com>
2018-07-03 21:15:18 +0900

    feat : add RequestWrite page of project
    add RequestWrite page of project

--- src/router/request.js
+++ src/router/request.js
@@ -1,7 +1,19 @@
 import RequestMain from '../pages/Request/RequestMain.vue'
+import RequestWrite from '../pages/Request/RequestWrite.vue'
+import Request from '../pages/Request/Request.vue'
 
 export const request = {
     path : '/request',
     name : 'Request',
-    component : RequestMain
+    component : Request,
+    children : [
+        {
+            path :'',
+            component : RequestMain
+        },
+        {
+            path : 'write',
+            component : RequestWrite
+        }
+    ]
 }
