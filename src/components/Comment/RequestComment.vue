045f10fc897adae08cfe5f79dbf45ac9d0ade396
SML <dltkdals2202@gmail.com>
2018-07-10 19:22:09 +0900

    feat : separate comment component from requestcomment component
    separate comment component from requestcomment component

--- src/components/Comment/RequestComment.vue
+++ src/components/Comment/RequestComment.vue
@@ -1,144 +1,78 @@
 <template>
-    <div class="req-comment">
-        <v-container>
-            <v-layout>
-                <div class="req-comment-count-section">
-                    <p class="req-comment-title">Comments</p>
-                    <!-- ***AXIOS*** -->
-                    <p class="req-comment-count">4</p>
-                </div>
-            </v-layout>
-            <v-layout>
-                <v-flex xs12 sm12 md12 lg12 class="req-comment-user-content-section">
-                    <div class="req-comment-user-content-inner-section">
-                        <!-- <textarea class="req-comment-user-content" :placeholder="comment"></textarea> -->
-                        <textarea class="req-comment-user-content"></textarea>
-                    </div>
-                    <div class="req-comment-btn-section">
-                        <button class="req-comment-btn">Comment</button>
-                    </div>
-                </v-flex>
-            </v-layout>
-            <v-layout>
-                <!-- ***AXIOS*** -->
-                <div class="req-comment-section">
-                    <div class="req-comment-user-icon">
-                        <v-icon class="req-comment-icon-person">person</v-icon>
-                    </div>
-                    <div class="req-comment-user-container">
-                        <div class="req-comment-user-info">
-                            <div class="req-comment-user-info-nickname req-comment-user-info-detail">
-                                
-                                <div>
-                                    <!--***AXIOS**-->
-                                    <p class="req-comment-user-info-nickname-text">SML</p>
-                                </div>
-                                <v-spacer/>
-                                <div>
-                                    <!--***AXIOS**-->
-                                    <p class="req-comment-user-info-btn">DELETE</p>
-                                    <p class="req-comment-user-info-btn req-comment-user-info-btn-reply">REPLY</p>
-                                </div>
-                            </div>
-                            <div class="req-comment-user-info-date-section req-comment-user-info-detail">
-                                <div>
-                                    <!--***AXIOS**-->
-                                    <p class="req-comment-user-info-date">01/01/2018</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="req-comment-user-comment-detail">
-                            <!--***AXIOS**-->
-                            <p class="req-comment-user-comment-detail-text">Hello World! I'm Sang min lee. ahahahahaHello World! I'm Sang min lee. ahahahahaHello World! I'm Sang min lee. ahahahahaHello World! I'm Sang min lee. ahahahahaHello World! I'm Sang min lee. ahahahahaHello World! I'm Sang min lee. ahahahahaHello World! I'm Sang min lee. ahahahaha</p>
-                            <!-- <div class="req-comment-user-comment-detail-space"></div> -->
-                        </div>
-                    </div>
-                </div>
-            </v-layout>
-            <v-layout>
-                <!-- ***AXIOS*** -->
-                <div class="req-comment-reply-section">
-                    <div class="req-comment-reply-icon">
-                        <v-icon>subdirectory_arrow_right</v-icon>
-                    </div>
-                    <div class="req-comment-user-icon">
-                        <v-icon class="req-comment-icon-person">person</v-icon>
-                    </div>
-                    <div class="req-comment-user-container">
-                        <div class="req-comment-user-info">
-                            <div class="req-comment-user-info-nickname req-comment-user-info-detail">
-                                <div>
-                                    <!--***AXIOS**-->
-                                    <p class="req-comment-user-info-nickname-text">KYG</p>
-                                </div>
-                                <v-spacer/>
-                                <div>
-                                    <!--***AXIOS**-->
-                                    <p class="req-comment-user-info-btn">DELETE</p>
-                                    <p class="req-comment-user-info-btn req-comment-user-info-btn-reply">REPLY</p>
-                                </div>
-                            </div>
-                            <div class="req-comment-user-info-date-section req-comment-user-info-detail">
-                                <div>
-                                    <!--***AXIOS**-->
-                                    <p class="req-comment-user-info-date">01/01/2018</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="req-comment-user-comment-detail">
-                            <!--***AXIOS**-->
-                            <p class="req-comment-user-comment-detail-text">ahahahahaHello World! I'm Sang min lee. ahahahahaHello World! I'm Sang min lee. ahahahahaHello World! I'm Sang min lee. ahahahahaHello World! I'm Sang min lee. ahahahaha</p>
-                        </div>
-                    </div>
-                </div>
-            </v-layout>
-            <v-layout>
-                    <div class="req-comment-reply-content-section">
-                        <div>
-                            <v-icon>subdirectory_arrow_right</v-icon>
-                        </div>
-                        <div class="req-comment-reply-content-section-inner">
-                            <!-- <textarea class="req-comment-user-content" :placeholder="reply"></textarea> -->
-                            <textarea class="req-comment-user-content"></textarea>
-                        </div>
-                    </div>
-                    <div class="req-comment-reply-btn-section">
-                        <button class="req-comment-reply-btn">Reply</button>
-                    </div>
-            </v-layout>
-        </v-container>
-    </div>
+  <div class="req-comment">
+    <v-container>
+      <v-layout>
+        <div class="req-comment-count-section">
+          <p class="req-comment-title">Comments</p>
+          <!-- ***AXIOS*** -->
+          <p class="req-comment-count">4</p>
+        </div>
+      </v-layout>
+      <v-layout>
+        <v-flex xs12 sm12 md12 lg12 class="req-comment-user-content-section">
+          <div class="req-comment-user-content-inner-section">
+            <!-- <textarea class="req-comment-user-content" :placeholder="comment"></textarea> -->
+            <textarea class="req-comment-user-content"></textarea>
+          </div>
+          <div class="req-comment-btn-section">
+            <button class="req-comment-btn">Comment</button>
+          </div>
+        </v-flex>
+      </v-layout>
+      <div class="req-comment-component">
+        <comment></comment>
+      </div>
+      
+      <div class="req-comment-reply-section">
+        <div class="req-comment-reply-icon">
+          <v-icon>subdirectory_arrow_right</v-icon>
+        </div>
+        <comment class="req-comment-reply"></comment>
+      </div>
+
+      <v-layout>
+        <div class="req-comment-reply-content-section">
+          <div>
+            <v-icon>subdirectory_arrow_right</v-icon>
+          </div>
+          <div class="req-comment-reply-content-section-inner">
+            <!-- <textarea class="req-comment-user-content" :placeholder="reply"></textarea> -->
+            <textarea class="req-comment-user-content"></textarea>
+          </div>
+        </div>
+        <div class="req-comment-reply-btn-section">
+          <button class="req-comment-reply-btn">Reply</button>
+        </div>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
+
 <script>
 // import Reply from './Reply'
+import Comment from "./Comment";
 export default {
   data() {
     return {
       comment: "Please input comment...",
       reply: "Please input reply..."
     };
+  },
+  components: {
+    Comment
   }
 };
 </script>
+
 <style lang='scss' scoped>
 @import "styles/common.scss";
-$button-width-height: 95px;
 $remain: 0px;
-$comment-height: 97px;
-$reply-height: 45px;
-
 p {
   display: inline;
   font-size: 15px;
 }
+
 .req-comment {
-  &-icon {
-    &-person {
-      width: 70px;
-      height: 70px;
-      font-size: 45px;
-    }
-  }
   &-count {
     padding-left: $inner-padding;
     &-section {
@@ -177,7 +111,6 @@ p {
         height: 100%;
       }
     }
-
     &-info {
       width: 100%;
       height: 70px;
@@ -210,6 +143,13 @@ p {
       }
     }
   }
+  &-component {
+    width: $remain;
+    border-bottom: 1px solid $grey-text;
+    @include media("(max-width : 620px)") {
+      width: 100%;
+    }
+  }
   &-btn {
     height: $button-width-height;
     width: $button-width-height;
@@ -217,15 +157,6 @@ p {
     border-radius: 5px;
     @include media("(max-width : 620px)") {
       height: 60px;
-    }
-  }
-  &-section {
-    width: $remain;
-    min-height: $comment-height;
-    border-bottom: 1px solid $grey-text;
-    display: flex;
-    @include media("(max-width : 620px)") {
-      width: 100%;
     }
   }
 
@@ -259,6 +190,7 @@ p {
     }
   }
 }
+
 .req {
   &-comment {
     &-reply {
@@ -280,13 +212,16 @@ p {
         }
       }
       &-section {
-        @extend .req-comment-section;
+        width: $remain;
+        border-bottom: 1px solid $grey-text;
         padding-left: 45px;
+        display: flex;
         @include media("(max-width : 620px)") {
+          width: 100%;
           padding-left: 0px;
         }
+        
       }
-
       &-btn {
         @extend .req-comment-btn;
         height: 45px;
