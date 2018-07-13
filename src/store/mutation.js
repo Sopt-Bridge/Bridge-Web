5d600972f7c218fd08ec697f3bad25c2bddc9c28
SML <dltkdals2202@gmail.com>
2018-07-13 12:28:40 +0900

    feat : title, subtitle tag를 mainposter에 추가
    title, subtitle tag를 mainposter에 추가

--- src/store/mutation.js
+++ src/store/mutation.js
@@ -1,7 +1,4 @@
 export const homeMutations = {
-  getHomeNowtrend(state, payload) {
-    console.log("getHomeNowtrend");
-  },
   presentHomeNowtrend(state, payload) {
     state.nowTrend = payload;
     console.log(payload);
